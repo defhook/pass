b653df2953a0207a76f76a5b505ecd34f2c6499a
defhook <defhook152@gmail.com>
2022-07-03 13:12:24 -0700

    updated about section with containers

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,33 @@
 import React from "react";
-import coverImage from "../../assets/main/polygonmap.jpg";
+import coverImage from "../../assets/main/abstractmap.jpg";
+import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Who are we?</h1>
-      <img
-        src={coverImage}
-        className="my-2"
-        style={{ width: "70%" }}
-        alt="cover"
-      />
-      <div className="my-2">
-        <p>
-          We are thrill seekers that are focused on getting the most of our
-          adrenaline rush! In 2022, a group of friends decided to create an easy and simple way of viewing wait times for theme parks. We are a small group that updates this site. If you have any questions feel free to reach out. 
-        </p>
-      </div>
+      <Container Fluid>
+        <Row>
+          <Col sm={8}>
+          <img
+            src={coverImage}
+            className="my-2"
+            style={{ width: "100%" }}
+            alt="cover"
+          />
+          </Col>
+          <Col>
+          <div className="my-2">
+            <p>
+              We are thrill seekers that are focused on getting the most of our
+              adrenaline rush! In 2022, a group of friends decided to create an
+              easy and simple way of viewing wait times for theme parks. We are
+              a small group that updates this site. If you have any questions
+              feel free to reach out.
+            </p>
+          </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,47 +1,43 @@
 import React from "react";
-import { Form, Nav, Button, Container, Row, Col, } from "react-bootstrap";
+import { Form, Nav, Button, Container, Row, Col } from "react-bootstrap";
 
 function Navigation() {
   return (
     <Nav>
       <div className="Nav">
-        <Row>
-          <Col>
-            <h1>Pass!</h1>
-          </Col>
-          <Nav className="Nav-header">
-            <Container fluid>
-              <Form>
-                <Row>
-                  <Col xs="4">
-                    <Form.Group controlId="formEmail">
-                      <Form.Label>User Id</Form.Label>
-                      <Form.Control
-                        type="email"
-                        placeholder="example@email.com"
-                      />
-                      <Form.Text className="text-muted" />
-                      Please log in for a personalized experience!
-                    </Form.Group>
-                  </Col>
-                  <Col xs="4">
-                    <Form.Group controlId="formPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="Password" placeholder="Password" />
-                      <Form.Text className="text-muted" />
-                    </Form.Group>
-                  </Col>
-                  <Col xs="3">
+        <Nav className="Nav-header">
+          <Container fluid>
+            <Form>
+              <Row>
+                <Col>
+                  <h1 class="neonText">Pass!</h1>
+                </Col>
+                <Col xs="4">
+                  <Form.Group controlId="formEmail">
+                    <Form.Label>User Id</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="example@email.com"
+                    />
+                    <Form.Text className="text-muted" />
+                  </Form.Group>
+                </Col>
+                <Col xs="4">
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="Password" placeholder="Password" />
+                    <Form.Text className="text-muted" />
+                  </Form.Group>
+                </Col>
+                <Col xs="4">
                   <Button variant="primary" type="submit">
-                    {" "}
-                    Log In{" "}
+                    Log In
                   </Button>
-                  </Col>
-                </Row>
-              </Form>
-            </Container>
-          </Nav>
-        </Row>
+                </Col>
+              </Row>
+            </Form>
+          </Container>
+        </Nav>
       </div>
     </Nav>
   );

ff1b8777a4be108a4db500f33a1d8e839702d9c3
defhook <defhook152@gmail.com>
2022-07-03 17:44:07 -0700

    removed flicker from background/story

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,27 +7,27 @@ function About() {
     <section className="my-5 about-me">
       <Container Fluid>
         <Row>
-        <h1 class="neonText">Pass</h1>
+          <h1 class="neonText">Pass</h1>
         </Row>
         <Row>
           <Col sm={8}>
-          <img
-            src={coverImage}
-            className="my-2"
-            style={{ width: "100%" }}
-            alt="cover"
-          />
+              <img
+                src={coverImage}
+                className="my-2"
+                style={{ width: "100%" }}
+                alt="cover"
+              />
           </Col>
           <Col>
-          <div className="my-2">
-            <p class="aboutMe">
-              We're thrill seekers that are focused on getting the most of our
-              adrenaline rush! In 2022, we decided to create an
-              easy and simple way of viewing wait times for theme parks. We're
-              a small group that updates this site. If you have any questions
-              feel free to reach out.
-            </p>
-          </div>
+            <div className="my-2">
+              <p class="aboutMe">
+                We're thrill seekers that are focused on getting the most of our
+                adrenaline rush! In 2022, we decided to create an easy and
+                simple way of viewing wait times for theme parks. We're a small
+                group that updates this site. If you have any questions feel
+                free to reach out.
+              </p>
+            </div>
           </Col>
         </Row>
       </Container>

695db22669d59559c656bf299efde20452189bcb
defhook <defhook152@gmail.com>
2022-07-06 09:16:13 -0700

    updated userpage with neon/flicker and stacked input fields/buttons

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -8,12 +8,12 @@ import { Stack, Container, Col, Row } from "react-bootstrap";
 
 import "../custom.css";
 
-export const Grid = styled.div`
-  display: grid;
-  grid-gap: 35px;
-  grid-template-columns: repeat(3, minmax(0, 1fr));
-  margin: 80px;
-`;
+// export const Grid = styled.div`
+//   display: grid;
+//   grid-gap: 35px;
+//   grid-template-columns: repeat(3, minmax(0, 1fr));
+//   margin: 80px;
+// `;
 export const CenteredText = styled.div`
   text-align: center;
 `;
@@ -56,48 +56,50 @@ function User() {
   };
 
   return (
-    <Container className="blueBackground">
+    <Container Fluid className="blueBackground">
       <div className="user-box">
-        <CenteredText>Create an Account</CenteredText>
+        <h1 class="neonText">Create an Account!</h1>
       </div>
-      <Col>
-        <Row>
+      <Row>
+        <Col xs="6">
           <img
             circle
             src={require("../assets/images/user-photo.png")}
             alt=""
             style={{
-              height: "250px",
+              height: "550px",
+              width: "550px",
             }}
           ></img>
-        </Row>
-        <Row>
-        <Stack gap={2} ClassName="col-md-5 mx-auto">
-          <div className="new-line">
-            <input
-              name="email"
-              value={signupForm.email}
-              onChange={handleInputChange}
-              type="text"
-              placeholder="Email Address"
-            ></input>
-          </div>
-          <div className="new-line">
-            <input
-              name="password"
-              value={signupForm.password}
-              onChange={handleInputChange}
-              type="password"
-              placeholder="Password"
-            ></input>
-          </div>
-
-          <button className="button-color" onClick={createUser}>
-            Sign Up!
-          </button>
-        </Stack>
-        </Row>
-      </Col>
+        </Col>
+        <Col md={{ offset: 1 }}>
+          <Stack gap={3} ClassName="col-md-5 mx-auto">
+            <div>
+              <input
+                name="email"
+                value={signupForm.email}
+                onChange={handleInputChange}
+                type="text"
+                placeholder="Email Address"
+              ></input>
+            </div>
+            <div>
+              <input
+                name="password"
+                value={signupForm.password}
+                onChange={handleInputChange}
+                type="password"
+                placeholder="Password"
+              ></input>
+            </div>
+            <div>
+              <button className="button-color" onClick={createUser}>
+                Sign Up!
+              </button>
+            </div>
+          </Stack>
+        </Col>
+      </Row>
     </Container>
   );
 }

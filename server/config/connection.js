6deb69f94f6ab80e5c7ca11948f56830959914b8
unknown <kikridu@gmail.com>
2022-07-05 23:04:10 -0700

    changes to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Pass', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pass', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;

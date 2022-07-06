0463037d389ec31c155d9d0610875a62b5e320da
unknown <kikridu@gmail.com>
2022-07-05 23:00:00 -0700

    last commit of the day

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = new ApolloServer({
 const app = express();
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
